19c8610ab5ee035d41eb9f967b41c4e1d2bf70e8
radwan <radwan@radwans-MacBook-Pro.local>
2021-04-22 23:30:09 +0200

    adding api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json }  do
+    namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      
+    end
+  end
 end
