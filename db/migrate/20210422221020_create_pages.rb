d6d08cb31467ddd641b76f7c0b6ef57551f6abfa
radwan <radwan@radwans-MacBook-Pro.local>
2021-04-23 02:24:50 +0200

    modifiy database configuration and model migration

--- db/migrate/20210422221020_create_pages.rb
+++ db/migrate/20210422221020_create_pages.rb
@@ -1,11 +1,12 @@
 class CreatePages < ActiveRecord::Migration[6.1]
   def change
     create_table :pages do |t|
-      t.bigint :id
-      t.string :url
-      t.string :shortenUrl
+      t.string :url,           null: false, unique: true
+      t.string :shortenUrl,    null: false, unique: true
 
       t.timestamps
     end
+
+    change_column :pages, :id, :bigint
   end
 end
